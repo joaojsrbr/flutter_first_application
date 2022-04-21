e4642f96ebc4ba6121bd01763ded0400ad8ee914
João Vitor <joaovitor.jsr@gmail.com>
2022-04-21 11:44:20 -0300

    fix colorTheme and add setColor

--- lib/theme/dark_theme_provider.dart
+++ lib/theme/dark_theme_provider.dart
@@ -4,10 +4,10 @@ import 'package:void_01/theme/shared_preferences.dart';
 class DarkThemeProvider with ChangeNotifier {
   DarkThemePreference darkThemePreference = DarkThemePreference();
   bool _darkTheme = false;
-  int _colorTheme = 0;
+  String _colorTheme = "#ffe91e63";
 
-  int get colorTheme => _colorTheme;
   bool get darkTheme => _darkTheme;
+  String get colorTheme => _colorTheme;
 
   set darkTheme(bool value) {
     _darkTheme = value;
@@ -15,7 +15,7 @@ class DarkThemeProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  set colorTheme(int value) {
+  set colorTheme(String value) {
     _colorTheme = value;
     darkThemePreference.setColor(value);
     notifyListeners();
--- lib/theme/shared_preferences.dart
+++ lib/theme/shared_preferences.dart
@@ -1,20 +1,28 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:shared_preferences/shared_preferences.dart';
 
 class DarkThemePreference {
   static const THEME_STATUS = "THEMESTATUS";
+  static const COLOR_STATUS = "COLORSTATUS";
 
   setDarkTheme(bool value) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool(THEME_STATUS, value);
   }
 
-  setColor(int value) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setInt(THEME_STATUS, value);
+  setColor(String value) async {
+    SharedPreferences prefs1 = await SharedPreferences.getInstance();
+    prefs1.setString(COLOR_STATUS, value);
   }
 
   Future<bool> getTheme() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     return prefs.getBool(THEME_STATUS) ?? false;
+  }
+
+  Future<String> getColor() async {
+    SharedPreferences prefs1 = await SharedPreferences.getInstance();
+    return prefs1.getString(COLOR_STATUS) ?? "";
   }
 }
