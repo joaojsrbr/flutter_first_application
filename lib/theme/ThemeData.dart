7d09bfd627956caba99e0207202d6073869cdf64
João Vitor <joaovitor.jsr@gmail.com>
2022-04-21 11:45:59 -0300

    brightness, textTheme e colorSchemeSeed adicionado

--- lib/theme/ThemeData.dart
+++ lib/theme/ThemeData.dart
@@ -1,15 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:void_01/theme/text_theme.dart';
+// ignore_for_file: file_names
 
-import 'lib_color_schemes.g.dart';
+import 'package:flutter/material.dart';
+import 'package:void_01/theme/hex_color.dart';
+import 'package:void_01/theme/text_theme.dart';
 
 class Styles {
   static ThemeData themeData(
-      bool isDarkTheme, BuildContext context, int color1) {
+      bool isDarkTheme, BuildContext context, String color1) {
+    print(color1);
     return ThemeData(
-        useMaterial3: true,
-        textTheme: Texttheme1(),
-        colorScheme:
-            isDarkTheme ? TabelaColorDark[color1] : TabelaColorlight[color1]);
+      useMaterial3: true,
+      textTheme: Texttheme1(context),
+      brightness: isDarkTheme ? Brightness.dark : Brightness.light,
+      colorSchemeSeed: hexToColor(color1),
+
+      // colorScheme:
+      //     isDarkTheme ? TabelaColorDark[color1] : TabelaColorlight[color1]
+    );
   }
 }
