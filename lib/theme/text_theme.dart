d147af7b3ed0959dfa4ad45af5e836d3873b31ea
João Vitor <joaovitor.jsr@gmail.com>
2022-04-21 11:45:03 -0300

    hexToColor

--- lib/theme/text_theme.dart
+++ lib/theme/text_theme.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Texttheme1() => TextTheme(
+Texttheme1(context) => TextTheme(
       caption: GoogleFonts.montserrat(
         fontSize: 12,
         fontWeight: FontWeight.w400,
